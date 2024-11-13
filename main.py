86b3e20d1c0bbe90894673bc6ef9e56ac7180b9f
Danis33 <daniscadriev2006@gmail.com>
2024-11-13 20:51:19 +0300

    Master

--- main.py
+++ main.py
@@ -133,7 +133,8 @@ def schedule_reminder(chat_id, task_index, reminder_time):
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("mark_task_"))
-
+def ask_for_mark_task(call):
+    pass
 
 
 @bot.message_handler(func=lambda message: message.text == "Удалить задание")
